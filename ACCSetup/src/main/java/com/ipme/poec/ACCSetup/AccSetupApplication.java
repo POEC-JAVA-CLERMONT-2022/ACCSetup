31ae033b4fdfff299c5ee6477be716abb60ce1f5
CormagLCC <43845400+CormagLCC@users.noreply.github.com>
2022-03-09 15:51:33 +0100

    Premier upload structure projet Springboot

---
+++ ACCSetup/src/main/java/com/ipme/poec/ACCSetup/AccSetupApplication.java
@@ -0,0 +1,13 @@
+package com.ipme.poec.ACCSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccSetupApplication.class, args);
+	}
+
+}
---
+++ ACCSetup/src/test/java/com/ipme/poec/ACCSetup/AccSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipme.poec.ACCSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
