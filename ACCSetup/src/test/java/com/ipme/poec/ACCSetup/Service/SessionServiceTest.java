aae6accd6c87934858f86a9ff5edac9889b4f34e
vincent <baidem@gmail.com>
2022-04-14 14:34:06 +0200

    1er jet

---
+++ ACCSetup/src/test/java/com/ipme/poec/ACCSetup/Service/SessionServiceTest.java
@@ -0,0 +1,36 @@
+package com.ipme.poec.ACCSetup.Service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ipme.poec.ACCSetup.Model.Session;
+import com.ipme.poec.ACCSetup.Repository.SessionRepository;
+
+@ExtendWith(MockitoExtension.class)
+class SessionServiceTest {
+	
+	@Mock private SessionRepository mockedSessionRepository;
+	
+	@Autowired
+	private SessionService sessionService;
+	
+
+	@Test
+	void testCreateSession() {
+		
+		when(mockedSessionRepository.save(Mockito.any(Session.class))).thenReturn(new Session(0, null, null, null, null, null));
+		Session testSession = sessionService.createSession(null, null, null, null, null);
+		// test null
+		assertNotNull(testSession);
+		
+		fail("Not yet implemented");
+	}
+
+}
