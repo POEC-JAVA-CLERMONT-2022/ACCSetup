beaa8d44b303486552cb0a47e4e138d1bb3b83e8
vincent <baidem@gmail.com>
2022-04-19 16:11:26 +0200

    thymeleaf pages : index.html car-list.html

--- ACCSetup/src/main/java/com/ipme/poec/ACCSetup/Controller/CarController.java
+++ ACCSetup/src/main/java/com/ipme/poec/ACCSetup/Controller/CarController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("api/cars")
 public class CarController {
 
 
---
+++ ACCSetup/src/main/java/com/ipme/poec/ACCSetup/Controller/PublicController.java
@@ -0,0 +1,31 @@
+package com.ipme.poec.ACCSetup.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ipme.poec.ACCSetup.Service.SessionService;
+
+@Controller
+public class PublicController {
+	
+	@Autowired
+	private SessionService sessionService;
+	
+	@GetMapping("/")
+	public ModelAndView index( ) {
+		return new ModelAndView("index");
+	}
+
+	@GetMapping("/car-list")
+	public ModelAndView carList( ) {
+		ModelAndView mAV = new ModelAndView("cars/car-list");
+		
+		mAV.addObject("cars", sessionService.findAllCars());
+
+        return mAV;
+	}		
+	
+	
+}
