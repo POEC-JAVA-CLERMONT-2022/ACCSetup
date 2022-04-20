43d8082e368d537f705852ec46f6fd9f66fbaefe
vincent <baidem@gmail.com>
2022-04-20 10:54:23 +0200

    ajout de track-list.html et weather-list.html

--- ACCSetup/src/main/java/com/ipme/poec/ACCSetup/Controller/PublicController.java
+++ ACCSetup/src/main/java/com/ipme/poec/ACCSetup/Controller/PublicController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ipme.poec.ACCSetup.Service.SessionService;
+import com.ipme.poec.ACCSetup.Service.SetupService;
 
 @Controller
 public class PublicController {
 	
 	@Autowired
 	private SessionService sessionService;
+	
+	@Autowired
+	private SetupService setupService;
 	
 	@GetMapping("/")
 	public ModelAndView index( ) {
@@ -32,6 +36,15 @@ public class PublicController {
 		ModelAndView mAV = new ModelAndView("tracks/track-list");
 		
 		mAV.addObject("tracks", sessionService.findAllTracks());
+
+        return mAV;
+	}
+	
+	@GetMapping("/weather-list")
+	public ModelAndView weatherList( ) {
+		ModelAndView mAV = new ModelAndView("weathers/weather-list");
+		
+		mAV.addObject("weathers", setupService.findAllWeathers());
 
         return mAV;
 	}
